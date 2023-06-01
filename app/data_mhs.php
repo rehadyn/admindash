a6e7697227fc066b0f9a7cea34609f61bfe00d9a
Reza Hadiwijaya Dynasti <ppg067@unm.ac.id>
2023-06-01 15:40:51 +0800

    Update formatter

--- app/data_mhs.php
+++ app/data_mhs.php
@@ -1,69 +1,87 @@
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-             
-              <!-- /.card-header -->
-              
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">DataTable with default features</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">DataTable with default features</h3>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal-default">
+              Edit
+            </button>
+            <br></br>
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>NIM</th>
+                  <th>Nama</th>
+                  <th>Semester</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                $no = 0;
+                $query = mysqli_query($koneksi, "SELECT * FROM tb_mhs");
+                while ($data = mysqli_fetch_array($query)) {
+                  $no++
+                ?>
                   <tr>
-                    <th>No</th>
-                    <th>NIM</th>
-                    <th>Nama</th>
-                    <th>Semester</th>
-                    <th>Action</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                    <?php
-                    $no = 0;
-                    $query = mysqli_query($koneksi, "SELECT * FROM tb_mhs");
-                    while ($data = mysqli_fetch_array($query)) {
-                        $no++
-                    ?>
-                  <tr>
-                    <td width ='5%'><?php echo $no ?></td>
+                    <td width='5%'><?php echo $no ?></td>
                     <td><?php echo $data['NIM']; ?></td>
                     <td><?php echo $data['Nama']; ?></td>
                     <td><?php echo $data['Semester']; ?></td>
                     <td><a href="../conf/curd.php" class="btn btn-sm btn-danger">Hapus</a></td>
                   </tr>
-                  <?php } ?>
-                  </tbody>
-                  <tfoot>
-                  <tr>
-                    <th>No</th>
-                    <th>NIM</th>
-                    <th>Nama</th>
-                    <th>Semester</th>
-                    <th>Action</th>
-                  </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+                <?php } ?>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th>No</th>
+                  <th>NIM</th>
+                  <th>Nama</th>
+                  <th>Semester</th>
+                  <th>Action</th>
+                </tr>
+              </tfoot>
+            </table>
           </div>
-          <!-- /.col -->
+          <!-- /.card-body -->
         </div>
-        <!-- /.row -->
+        <!-- /.card -->
       </div>
-      <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
   </div>
+  <!-- /.container-fluid -->
+</section>
+<div class="modal fade" id="modal-default">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Default Modal</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.content -->
+</div>
