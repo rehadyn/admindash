c9ca6db0ad3b602e98324dfbd076f214d0934c1b
Reza Hadiwijaya Dynasti <ppg067@unm.ac.id>
2023-06-01 15:41:27 +0800

    Format

--- app/index.php
+++ app/index.php
@@ -1,66 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php 
+<?php
 session_start();
-if (!$_SESSION['username']){
+if (!$_SESSION['username']) {
   header("location:../index.php?session=exp");
 }
-include('header.php'); 
+include('header.php');
 ?>
 <?php include('../conf/config.php'); ?>
 
 
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!-- Preloader -->
- <?php include('preloader.php'); ?>
-  <!-- Navbar -->
-  <?php include('navbar.php'); ?>
-  <!-- /.navbar -->
+    <!-- Preloader -->
+    <?php include('preloader.php'); ?>
+    <!-- Navbar -->
+    <?php include('navbar.php'); ?>
+    <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <?php include('logo.php'); ?>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <?php include('logo.php'); ?>
 
-    <!-- Sidebar -->
-  <?php include('sidebar.php'); ?>
-    <!-- /.sidebar -->
-  </aside>
+      <!-- Sidebar -->
+      <?php include('sidebar.php'); ?>
+      <!-- /.sidebar -->
+    </aside>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <?php include('content_header.php'); ?>
-    <!-- /.content-header -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <?php include('content_header.php'); ?>
+      <!-- /.content-header -->
 
-    <!-- Main content -->
-    <?php 
-if (isset($_GET['page'])){
-  if ($_GET['page'] == 'dashboard') {
-    include('content.php');
-  } else if ($_GET['page'] == 'data-mhs') {
-    include('data_mhs.php');
-  } else {
-    include('content.php');
-  }
-} else {
-  include('content.php');
-}
-?>
+      <!-- Main content -->
+      <?php
+      if (isset($_GET['page'])) {
+        if ($_GET['page'] == 'dashboard') {
+          include('content.php');
+        } else if ($_GET['page'] == 'data-mhs') {
+          include('data_mhs.php');
+        } else {
+          include('content.php');
+        }
+      } else {
+        include('content.php');
+      }
+      ?>
 
-    <!-- /.content -->
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <?php include('footer.php'); ?>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
   </div>
-  <!-- /.content-wrapper -->
-  <?php include('footer.php'); ?>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+  <!-- ./wrapper -->
 </body>
+
 </html>
